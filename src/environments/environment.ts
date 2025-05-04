7394f13b36f55ce1f5655420413f47bcb6229f9d
Arindam Dey <92677226+Arindam443@users.noreply.github.com>
2025-05-05 02:27:30 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,7 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyD-RWzUWXePpghzVp9tsuUhaS4DHsj-qu0",
-        authDomain: "expense-tracker-32379.firebaseapp.com",
-        projectId: "expense-tracker-32379",
-        storageBucket: "expense-tracker-32379.firebasestorage.app",
-        messagingSenderId: "555592223611",
-        appId: "1:555592223611:web:ebd479f9031d0bc2034bde",
-        measurementId: "G-0TK5WMY9VC"
+
     }
   };
   
